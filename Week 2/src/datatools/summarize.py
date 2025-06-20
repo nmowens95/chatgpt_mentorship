39bdeadb5b6bd67de7bcb2b1a97b40cf91709208
nmowens95 <nmowens95@gmail.com>
2025-06-20 10:29:44 -0500

    Broke code up to more modular and dynamic pieces for when arguments are passed

--- Week 2/src/datatools/summarize.py
+++ Week 2/src/datatools/summarize.py
@@ -10,15 +10,49 @@ logging.basicConfig(
 
 logger = logging.getLogger()
 
+def include_dtype_fx(col: pd.Series) -> dict:
+    col_dtype = str(col.dtypes)
+    return {"dtype": col_dtype}
+
+def include_null_fx(col: pd.Series) -> dict:
+    col_isna = round(100 * (col.isna().sum() / len(col)), 1)
+    return {"isna": col_isna}
+
+def include_mode_fx(column: pd.Series) -> dict:
+    col_mode = column.mode()
+    return {"mode": col_mode.to_list()}
+
 def summarize_df(df, include_dtype=True, include_null=True, include_mode=True):
+    """
+    Summarize a pandas DataFrame by generating stats per column.
+
+    Parameters:
+        df (pd.DataFrame): The DataFrame to summarize.
+        include_dtype (bool): Include data type of columns.
+        include_null (bool): Include percentage of null values.
+        include_mode (bool): Include mode (most frequent values) of columns.
+
+    Returns:
+        dict: Summary dictionary with column statistics.
+    """
     if not isinstance(df, pd.DataFrame):
         logger.info("The data frame trying to be passed is not matching an actual df type")
         raise TypeError("Value passed is not a dataframe type")
     
     if df.empty:
         logger.info("Value being passed has no values")
-        raise ValueError("Empty DataFrame pass")          
-        
+        raise ValueError("Empty DataFrame pass")
+
+    # Dyncamically collect necessary functions
+    features = []
+    if include_dtype:
+        features.append(include_dtype_fx)      
+
+    if include_null:
+        features.append(include_null_fx)
+
+    if include_mode:
+        features.append(include_mode_fx) 
     
     summary = {"columns": []}
     for column in df.columns:
@@ -28,16 +62,9 @@ def summarize_df(df, include_dtype=True, include_null=True, include_mode=True):
 
         column_summary = {"name": df[column].name}
 
-        if include_dtype:
-            column_summary["dtype"] = str(df[column].dtypes) # Convert to a string
-
-        if include_null:
-            column_summary["isna"] = round(100 * (df[column].isna().sum() / len(df)), 1)
-
-        if include_mode:
-            col_mode = df[column].mode()
-            column_summary["mode"] = col_mode.to_list()
-
+        for func in features:
+            column_summary.update(func(df[column]))
+    
         summary["columns"].append(column_summary)
 
     return summary
