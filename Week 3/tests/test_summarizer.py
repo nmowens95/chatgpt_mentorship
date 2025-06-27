5cba88d5ec9fc2b5567f65ef7190ac02e49d3cd2
nmowens95 <nmowens95@gmail.com>
2025-06-26 21:25:22 -0500

    Added some test and messed around with pytest.raise

--- Week 3/tests/test_summarizer.py
+++ Week 3/tests/test_summarizer.py
@@ -3,6 +3,8 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from features import dtype_summary, null_summary, mode_summary
+from file_loader import CustomFileReadError, load_csv_files
+import pytest
 
 def test_dtype_summary():
     data = {
@@ -14,3 +16,31 @@ def test_dtype_summary():
     result = dtype_summary(df) 
     
     assert result["age"] == 'int64'
+
+def test_null_summary():
+    data = {
+        "name": ["John", "Karen", None, "Rachel", None],
+        "age": [20, 55, 60, None, 15]
+    }
+    df = pd.DataFrame(data)
+    result = null_summary(df)
+
+    assert result["name"] > 0
+    assert result["age"] > 0
+
+def test_mode_summary():
+    data = {
+        "name": ["John", "Karen", None, "Rachel", None],
+        "age": [20, 55, 60, None, 15]
+    }
+    df = pd.DataFrame(data)
+    result = mode_summary(df)
+
+    assert result["name"] == "John"
+    assert result["age"] != None
+
+
+def test_CustomFileReadError():
+    with pytest.raises(CustomFileReadError):
+        load_csv_files("fake_dir")
+        
