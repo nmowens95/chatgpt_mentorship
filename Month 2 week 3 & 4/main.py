0e07d3c5edec960771db8b05f9c19f2b5bd26040
nmowens95 <nmowens95@gmail.com>
2025-07-30 17:49:29 -0500

    Just small changes to main for now to get it actually functioning

--- Month 2 week 3 & 4/main.py
+++ Month 2 week 3 & 4/main.py
@@ -1,5 +1,19 @@
 import pandas as pd
+from src.pipeline.transform_to_silver import bronze_to_silver
+from src.pipeline.load_to_postgres import load_gold_to_postgres
+from src.utils.logger_config import logger_setup
 
-df = pd.read_csv("data/bronze/claims.csv")
+logger = logger_setup()
 
-print(df.head())
+def main():
+    try:
+        logger.info("Pipeline starting...")
+        bronze_to_silver("data/bronze")
+        load_gold_to_postgres("data/silver")
+    except Exception as e:
+        logger.error(f"Error in pipeline {e}")
+    finally:
+        logger.info("Pipeline complete!")
+
+if __name__ == "__main__":
+    main()
